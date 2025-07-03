dbf25c53e6c574ce1eda3b0041756d95a0073c09
yoshakit <yoshakit@amazon.co.jp>
2025-07-03 12:39:46 +0900

    fix

--- frontend/src/amplify-config.ts
+++ frontend/src/amplify-config.ts
@@ -4,10 +4,8 @@ export const configureAmplify = () => {
   Amplify.configure({
     Auth: {
       Cognito: {
-        region: import.meta.env.VITE_APP_REGION,
-        userPoolId: import.meta.env.VITE_APP_USER_POOL_ID,
-        userPoolClientId: import.meta.env.VITE_APP_USER_POOL_WEB_CLIENT_ID,
-        identityPoolId: import.meta.env.VITE_APP_ID_POOL_ID,
+        userPoolId: String(import.meta.env.VITE_APP_USER_POOL_ID),
+        userPoolClientId: String(import.meta.env.VITE_APP_USER_POOL_WEB_CLIENT_ID)
       }
     }
   });
