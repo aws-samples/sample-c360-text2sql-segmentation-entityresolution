4791ce7b2fe3d57294e9a2a0b1dafcee8d1ca1ed
yoshakit <yoshakit@amazon.co.jp>
2025-07-03 12:39:46 +0900

    fix

--- lib/cognito.ts
+++ lib/cognito.ts
@@ -8,9 +8,6 @@ import { Construct } from 'constructs';
 export class Cognito extends Construct {
   readonly userPool: cognito.UserPool;
   readonly userPoolClient: cognito.UserPoolClient;
-  readonly identityPool: cognito.CfnIdentityPool;
-  readonly authenticatedRole: iam.Role;
-  readonly unauthenticatedRole: iam.Role;
 
   constructor(scope: Construct, id: string) {
     super(scope, id);
@@ -35,62 +32,8 @@ export class Cognito extends Construct {
       }
     });
 
-    // Creating Identity Pool
-    this.identityPool = new cognito.CfnIdentityPool(this, 'IdentityPool', {
-      allowUnauthenticatedIdentities: false,
-      cognitoIdentityProviders: [
-        {
-          clientId: this.userPoolClient.userPoolClientId,
-          providerName: this.userPool.userPoolProviderName
-        }
-      ]
-    });
-
-    // Creating IAM role for authenticated users
-    this.authenticatedRole = new iam.Role(this, 'CognitoAuthenticatedRole', {
-      assumedBy: new iam.FederatedPrincipal(
-        'cognito-identity.amazonaws.com',
-        {
-          StringEquals: {
-            'cognito-identity.amazonaws.com:aud': this.identityPool.ref
-          },
-          'ForAnyValue:StringLike': {
-            'cognito-identity.amazonaws.com:amr': 'authenticated'
-          }
-        },
-        'sts:AssumeRoleWithWebIdentity'
-      )
-    });
-
-    // Creating IAM role for unauthenticated users (minimal permissions)
-    this.unauthenticatedRole = new iam.Role(this, 'CognitoUnauthenticatedRole', {
-      assumedBy: new iam.FederatedPrincipal(
-        'cognito-identity.amazonaws.com',
-        {
-          StringEquals: {
-            'cognito-identity.amazonaws.com:aud': this.identityPool.ref
-          },
-          'ForAnyValue:StringLike': {
-            'cognito-identity.amazonaws.com:amr': 'unauthenticated'
-          }
-        },
-        'sts:AssumeRoleWithWebIdentity'
-      )
-    });
-
-    // Creating Identity Pool role attachment
-    new cognito.CfnIdentityPoolRoleAttachment(this, 'IdentityPoolRoleAttachment', {
-      identityPoolId: this.identityPool.ref,
-      roles: {
-        authenticated: this.authenticatedRole.roleArn,
-        unauthenticated: this.unauthenticatedRole.roleArn
-      }
-    });
-
     // 出力値の設定
     new cdk.CfnOutput(this, 'UserPoolId', { value: this.userPool.userPoolId });
     new cdk.CfnOutput(this, 'UserPoolClientId', { value: this.userPoolClient.userPoolClientId });
-    new cdk.CfnOutput(this, 'IdentityPoolId', { value: this.identityPool.ref });
-    new cdk.CfnOutput(this, 'AuthenticatedRoleArn', { value: this.authenticatedRole.roleArn });
   }
 }
